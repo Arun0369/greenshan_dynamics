ac189ebba136507e996ce2dcd27e6ccc1f738283
Arun E <arun0369@gmail.com>
2026-03-23 16:52:21 +0530

    Fix duplicate app config

--- greenshan_project/settings.py
+++ greenshan_project/settings.py
@@ -31,10 +31,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-
-    "greenshan",
     'greenshan.apps.GreenshanConfig',
 ]
+
 
 
 # =================================================
